08379a2b5da09e7d7edd540063eb673b81c87d82
Werner Kühn <werner_kuehn@gmx.de>
2023-02-02 13:36:02 +0100

    add spam move function

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Application for cleaning up your email account from spam mail based on a given list of email addresses.
 In order to easily enhance your blacklist you can create a folder "Blacklist" in your email account. If you add this folder into your \<filename\>.ini folder list then every email address in this folder gets added to the appropriate blacklist.
+For emails going into the Spam folder by mistake you define can the addresses in the whitelist file and they will get moved into the Inbox
 
 ## Installation
 
@@ -9,9 +10,10 @@ In order to easily enhance your blacklist you can create a folder "Blacklist" in
 
 - create the config file <filename>.ini based on my_account.ini.sample
 
-  - this config file can have a common blacklist filename in the DEFAULT part, as well as individual blacklist filenames for every account
+  - this config file can have a common blacklist and whitelist filename in the DEFAULT part, as well as an individual blacklist/whitelist filenames for every account
 
-  - required parameters are HOST, USERNAME, PASSWORD, FOLDER. If one of these parameters is missing the account will be ignored.
+  - required parameters for blacklisting are HOST, USERNAME, PASSWORD, FOLDER. If one of these parameters is missing the account will be ignored.
+  - required parameters for whitelisting are HOST, USERNAME, PASSWORD, FOLDER, INBOX. If one of these parameters is missing the account will be ignored.
 
 ## PRE-REQUISITE
 
--- spam_cleaner.py
+++ spam_cleaner.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 from imapclient import IMAPClient
 
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 # Enable logging
 logfile = "/var/log/" + \
@@ -23,7 +23,7 @@ class SpamCleaner():
 
     def __init__(self):
         args = self.__get_cli_arguments__()
-        # args.configfile = ['gmx.ini']	# for debugging purposes only
+        # args.configfile = ['gmx.ini']  # for debugging purposes only
         if args.configfile and Path(args.configfile[0]).is_file():
             self.prefs = self.__read_configuration__(args.configfile)
         else:
@@ -31,14 +31,16 @@ class SpamCleaner():
                 'No config file provided or config file not found. Use "{} -c <config.ini>"'.format(os.path.basename(__file__)))
             return
         for account in self.prefs:
-            self.cleanup(account)
+            if account != 'DEFAULT':
+                self.deleteSpam(account)
+                self.moveSpam(account)
 
-    def get_blacklist(self, blacklist_file):
-        if blacklist_file and Path(blacklist_file).is_file():
-            return open(blacklist_file, 'r').read().splitlines()
+    def get_black_or_white_list(self, list_file):
+        if list_file and Path(list_file).is_file():
+            return open(list_file, 'r').read().splitlines()
 
     def append_blacklist(self, blacklist_file, address):
-        blacklist = self.get_blacklist(blacklist_file)
+        blacklist = self.get_black_or_white_list(blacklist_file)
         if blacklist:
             blacklist.append(address)
             blacklist = list(set(blacklist))
@@ -69,8 +71,12 @@ class SpamCleaner():
                 preferences[sectionname][name] = value
         return preferences
 
-    def cleanup(self, account):
-        if set(['host', 'username', 'password', 'folder']).difference(set(self.prefs[account])):
+    def deleteSpam(self, account):
+        diff = set(['host', 'username', 'password', 'folder']
+                   ).difference(set(self.prefs[account]))
+        if diff:
+            logger.error(
+                'Missing parameter(s) {} in ini file for {}'.format(diff, account))
             return
         HOST = self.prefs[account]['host']
         USERNAME = self.prefs[account]['username']
@@ -78,9 +84,7 @@ class SpamCleaner():
         FOLDERLIST = self.prefs[account]['folder'].split(',')
         BLACKLISTFILE = self.prefs[account]['blacklist'] if 'blacklist' in self.prefs[
             account] else self.prefs['DEFAULT']['blacklist']
-        BLACKLIST = self.get_blacklist(BLACKLISTFILE)
-        if not BLACKLIST:
-            return
+        BLACKLIST = self.get_black_or_white_list(BLACKLISTFILE)
 
         ssl_context = ssl.create_default_context()
 
@@ -90,42 +94,103 @@ class SpamCleaner():
         # check if the certificate is trusted by a certificate authority
         ssl_context.verify_mode = ssl.CERT_REQUIRED
 
-        delete_count = 0
-        with IMAPClient(HOST, ssl_context=ssl_context) as server:
-            server.login(USERNAME, PASSWORD)
-            for folder in FOLDERLIST:
-                try:
-                    server.select_folder(folder.strip(), readonly=False)
-                except:
-                    print('Unknown Folder')
-                    return
-                messages = server.search('ALL')
-                for uid, message_data in server.fetch(messages, 'RFC822').items():
-                    email_message = email.message_from_bytes(
-                        message_data[b'RFC822'])
-                    if folder.strip().lower() == 'blacklist':
-                        address = email_message.get('From')
-                        self.append_blacklist(
-                            BLACKLISTFILE, address[address.index("<")+1:-1])
-                        server.delete_messages([uid])
-                        delete_count += 1
-                        logger.info("{} {} {} has been deleted".format(
-                            uid, email_message.get('From'), email_message.get('Subject')))
-                    if next((s for s in BLACKLIST if email_message.get('From') and s in email_message.get('From')), None):
-                        server.delete_messages([uid])
-                        delete_count += 1
-                        logger.info("{} {} {} has been deleted".format(
-                            uid, email_message.get('From'), email_message.get('Subject')))
-                server.close_folder()
-            if delete_count == 0:
-                logger.info(
-                    'No matching Emails found for {}!'.format(USERNAME))
-            elif delete_count == 1:
-                logger.info("{} Email has been deleted for {}!".format(
-                    delete_count, USERNAME))
-            else:
-                logger.info("{} Emails have been deleted for {}!".format(
-                    delete_count, USERNAME))
+        if BLACKLIST:
+            delete_count = 0
+            with IMAPClient(HOST, ssl_context=ssl_context) as server:
+                server.login(USERNAME, PASSWORD)
+                for folder in FOLDERLIST:
+                    try:
+                        server.select_folder(folder.strip(), readonly=False)
+                    except:
+                        print('Unknown Folder')
+                        return
+                    messages = server.search('ALL')
+                    for uid, message_data in server.fetch(messages, 'RFC822').items():
+                        email_message = email.message_from_bytes(
+                            message_data[b'RFC822'])
+                        if folder.strip().lower() == 'blacklist':
+                            address = email_message.get('From')
+                            if not address:
+                                for defect in email_message.defects:
+                                    type, address = defect.line.split(':', 1)
+                                    if type.strip() == 'From':
+                                        break
+                            if address:
+                                if '<' in address:
+                                    address = address[address.index("<")+1:-1]
+                                self.append_blacklist(BLACKLISTFILE, address)
+                                server.delete_messages([uid])
+                                delete_count += 1
+                                logger.info("{} {} {} has been deleted".format(
+                                    uid, address, email_message.get('Subject')))
+                        if next((s for s in BLACKLIST if email_message.get('From') and s in email_message.get('From')), None):
+                            server.delete_messages([uid])
+                            delete_count += 1
+                            logger.info("{} {} {} has been deleted".format(
+                                uid, email_message.get('From'), email_message.get('Subject')))
+                    server.close_folder()
+                if delete_count == 0:
+                    logger.info(
+                        'No matching Emails found for {}!'.format(USERNAME))
+                elif delete_count == 1:
+                    logger.info("{} Email has been deleted for {}!".format(
+                        delete_count, USERNAME))
+                else:
+                    logger.info("{} Emails have been deleted for {}!".format(
+                        delete_count, USERNAME))
+
+    def moveSpam(self, account):
+        diff = set(['host', 'username', 'password', 'folder',
+                   'inbox']).difference(set(self.prefs[account]))
+        if diff:
+            logger.error(
+                'Missing parameter(s) {} in ini file for {}'.format(diff, account))
+            return
+        HOST = self.prefs[account]['host']
+        USERNAME = self.prefs[account]['username']
+        PASSWORD = self.prefs[account]['password']
+        FOLDERLIST = self.prefs[account]['folder'].split(',')
+        INBOX = self.prefs[account]['inbox']
+        WHITELISTFILE = self.prefs[account]['whitelist'] if 'whitelist' in self.prefs[
+            account] else self.prefs['DEFAULT']['whitelist']
+        WHITELIST = self.get_black_or_white_list(WHITELISTFILE)
+
+        ssl_context = ssl.create_default_context()
+
+        # check if certificate hostname matches target hostname
+        ssl_context.check_hostname = True
+
+        # check if the certificate is trusted by a certificate authority
+        ssl_context.verify_mode = ssl.CERT_REQUIRED
+        if WHITELIST:
+            move_count = 0
+            with IMAPClient(HOST, ssl_context=ssl_context) as server:
+                server.login(USERNAME, PASSWORD)
+                for folder in FOLDERLIST:
+                    try:
+                        server.select_folder(folder.strip(), readonly=False)
+                    except:
+                        print('Unknown Folder')
+                        return
+                    messages = server.search('ALL')
+                    for uid, message_data in server.fetch(messages, 'RFC822').items():
+                        email_message = email.message_from_bytes(
+                            message_data[b'RFC822'])
+                        if next((s for s in WHITELIST if email_message.get('From') and s in email_message.get('From')), None):
+                            server.move([uid], INBOX)
+                            move_count += 1
+                            logger.info("{} {} {} has been moved".format(
+                                uid, email_message.get('From'), email_message.get('Subject')))
+                    server.close_folder()
+                if move_count == 0:
+                    logger.info(
+                        'No matching Emails found for {} to be moved!'.format(USERNAME))
+                elif move_count == 1:
+                    logger.info("{} Email has been moved for {}!".format(
+                        move_count, USERNAME))
+                else:
+                    logger.info("{} Emails have been moved for {}!".format(
+                        move_count, USERNAME))
 
 
 SpamCleaner()
